56308f205e6416858b9f4c170540b5fa4f1bbe3c
Nicolas BOULOGNE-CURRIEZ <nicolas.boulogne-curriez@epitech.eu>
2019-07-04 16:40:04 +0200

    [ADD] ReactNativeElements librairie

--- application/App.js
+++ application/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, ThemeProvider } from 'react-native-elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <ThemeProvider>
+      <Button title="Hey!" />
+    </ThemeProvider>
   );
 }
 
