4d99178e61ffd94fddb9f066ddf4a68c2c17ea39
Nicolas BOULOGNE-CURRIEZ <nicolas.boulogne-curriez@epitech.eu>
2019-07-08 17:03:22 +0200

    [ADD] Login form and checkinput password

--- application/src/Component/Connection/Login/Login.component.js
+++ application/src/Component/Connection/Login/Login.component.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, Input, Icon } from 'react-native-elements';
 import strings from '../../../../config/strings';
 import styles from './Login.component.style';
 
 class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleClick(event) {
+    if (this.state.password == '') {
+      console.log('mdr');
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Welcome to the login screen!</Text>
-        <Button title={strings.LOGIN} type="outline" />
+        <Input
+          autoCapitalize='none'
+          autoCorrect={false}
+          keyboardType='email-address'
+          returnKeyType='next'
+          onSubmitEditing={() => this.password.focus() }
+          blurOnSubmit={false}
+          placeholder='Email'
+          leftIcon={<Icon name='person' size={24} color='black' />}
+          onChangeText={(email) => this.setState({ email })}
+        />
+        <Input
+          ref={(input) => { this.password = input; }}
+          returnKeyType="go"
+          placeholder='Password'
+          secureTextEntry={true}
+          leftIcon={<Icon name='lock' size={24} color='black' />}
+          onChangeText={(password) => this.setState({ password })}
+        />
+        <Button title={strings.LOGIN} type="outline" onPress={() => this.handleClick('login')} />
       </View>
     )
   }
