8b8fd73a7f4dd43eabc50fa24341ccb9114a420a
Nicolas BOULOGNE-CURRIEZ <nicolas.boulogne-curriez@epitech.eu>
2019-07-08 15:16:37 +0200

    [FIX] path config file

--- application/src/Component/Connection/Login/Login.component.js
+++ application/src/Component/Connection/Login/Login.component.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import strings from '../../../../config/strings';
 import styles from './Login.component.style';
 
 class Login extends React.Component {
@@ -7,6 +9,7 @@ class Login extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Welcome to the login screen!</Text>
+        <Button title={strings.LOGIN} type="outline" />
       </View>
     )
   }
