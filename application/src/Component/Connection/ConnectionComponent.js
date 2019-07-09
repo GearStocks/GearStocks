56402cbbdde7c4f31ac29ef67b1c6f8576145fb0
Nicolas BOULOGNE-CURRIEZ <nicolas.boulogne-curriez@epitech.eu>
2019-07-09 15:25:42 +0200

    [FIX]test

--- application/src/Component/Connection/ConnectionComponent.js
+++ application/src/Component/Connection/ConnectionComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Login from "./Login/Login.component";
-import { RegisterComponent } from './RegisterComponent';
+import { RegisterComponent } from "./RegisterComponent";
 import { ForgotPasswordComponent } from './ForgotPasswordComponent';
 
 export default class ConnectionComponent extends React.Component {
