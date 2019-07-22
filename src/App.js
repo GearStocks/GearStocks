5f65b020be47d514ce738d949746f3fe7f43715a
Nicolas BOULOGNE-CURRIEZ <nicolas.boulogne-curriez@epitech.eu>
2019-07-22 13:35:57 +0200

    [FIX] Architecture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import React from 'react';
-import AppContainer from './AppNavigator';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import ConnectionComponent from './Component/Connection/ConnectionComponent';
+import RegisterComponent from './Component/Connection/Register/Register.component';
+//import ForgotPasswordComponent from './Component/Connection/ForgotPassword/ForgotPassword.component';
+
+const AppNavigator = createStackNavigator({
+  ConnectionComponent: {
+    screen: ConnectionComponent,
+    navigationOptions: {
+      header: null
+    }
+  },
+  RegisterComponent: {
+    screen: RegisterComponent,
+    navigationOptions: {
+      header: null
+    }
+  }
+  /*ForgotPasswordComponent: {
+    screen: ForgotPasswordComponent,
+    navigationOptions: {
+      header: null
+    }
+  }*/
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
 
 export default class App extends React.Component {
   render() {
