fcbae9d4720a756ba3e2045f8939c51ea7af273b
Nicolas BOULOGNE-CURRIEZ <nicolas.boulogne-curriez@epitech.eu>
2019-12-05 18:39:02 +0900

    (ADD) GridView

--- application/src/Component/Profil/UserInformations/UserInformations.component.style.js
+++ application/src/Component/Profil/UserInformations/UserInformations.component.style.js
@@ -1,3 +1,10 @@
+/**
+ * @author Nicolas  BOULOGNE-CURRIEZ <nicolas.boulogne-curriez@epitech.eu>
+ * @file Description
+ * @desc Created on 2019-12-05 5:50:51 pm
+ * @copyright GearStocks
+ */
+
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
