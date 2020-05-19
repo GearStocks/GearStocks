6acd9b07122845e8aa04dc5c27485da80a96c03e
Valentin LEBON <valentin.lebon@epitech.eu>
2020-05-19 13:27:44 +0100

    [UPDATE] use env file

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
-  loginUrl: 'http://localhost:8000/connect',
-  registerUrl: 'http://localhost:8000/register',
-  resetPasswordUrl: 'http://localhost:8000/resetPassword',
-  updateProfileUrl: 'http://localhost:8000/user',
-  updateEmailUrl: 'http://localhost:8000/user',
-  updatePasswordUrl: 'http://localhost:8000/user',
-  searchUrl: 'http://localhost:8000/search',
-  getItemUrl: 'http://localhost:8000/item'
+  loginUrl: 'http://' + process.env.API_ADDR  + ':' + process.env.API_PORT  + '/connect',
+  registerUrl: 'http://' + process.env.API_ADDR  + ':' + process.env.API_PORT  + '/register',
+  resetPasswordUrl: 'http://' + process.env.API_ADDR  + ':' + process.env.API_PORT  + '/resetPassword',
+  updateProfileUrl: 'http://' + process.env.API_ADDR  + ':' + process.env.API_PORT  + '/user',
+  updateEmailUrl: 'http://' + process.env.API_ADDR  + ':' + process.env.API_PORT  + '/user',
+  updatePasswordUrl: 'http://' + process.env.API_ADDR  + ':' + process.env.API_PORT  + '/user',
+  searchUrl: 'http://' + process.env.API_ADDR  + ':' + process.env.API_PORT  + '/search',
+  getItemUrl: 'http://' + process.env.API_ADDR  + ':' + process.env.API_PORT  + '/item'
 };
