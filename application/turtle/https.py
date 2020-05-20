aa9b5115a9f8fc0eda35c631fb51e9ff794b3325
Valentin LEBON <valentin.lebon@epitech.eu>
2020-05-20 22:26:32 +0100

    [TEST] fix App compilation

--- application/turtle/https.py
+++ application/turtle/https.py
@@ -1,5 +1,5 @@
 import http.server, ssl
 
-httpd = http.server.HTTPServer(('localhost', 4443), http.server.SimpleHTTPRequestHandler)
+httpd = http.server.HTTPServer(('localhost', 443), http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
 httpd.serve_forever()
