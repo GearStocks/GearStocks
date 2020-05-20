a09d6ae5a1cf28a529329183523ffa55f32d706f
Raphigo <raphael.sebaon@epitech.eu>
2020-05-20 14:54:58 +0200

    [ADD] mailer + route reset password. [FIX] json sent instead of string and error handeling

--- back/src/Main.cpp
+++ back/src/Main.cpp
@@ -5,25 +5,27 @@
 #include "rapidjson/document.h"
 #include "../include/Server.hpp"
 #include "../include/BddManager.hpp"
+#include "../include/Emailer.hpp"
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    int portEnv;
-
-    try {
-        portEnv = std::stoi(argv[1]);
-    } catch(std::exception e) {
-        std::cout << e.what() << ": Please make sure you provide a valid port as first argument." << std::endl;
-        return 1;
-    }
-
-    Pistache::Port port(portEnv);
-    Pistache::Address addr(Pistache::Ipv4::any(), port);
-    BddManager	*manager = new BddManager();
-    Server gearStockServ(addr, manager);
-
-    std::cout << "listening on : " << port << std::endl;
-    gearStockServ.start();
-
-    gearStockServ.shutdown();
+  int portEnv;
+  
+  try {
+    portEnv = std::stoi(argv[1]);
+  } catch(std::exception e) {
+    std::cout << e.what() << ": Please make sure you provide a valid port as first argument." << std::endl;
+    return 1;
+  }
+  
+  Pistache::Port port(portEnv);
+  Pistache::Address addr(Pistache::Ipv4::any(), port);
+  BddManager	*manager = new BddManager();
+  Emailer	*emailer = new Emailer();
+  Server gearStockServ(addr, manager, emailer);
+  
+  std::cout << "listening on : " << port << std::endl;
+  gearStockServ.start();
+  
+  gearStockServ.shutdown();
 }
