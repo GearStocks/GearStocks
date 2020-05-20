b41dcece874aaa597af583c822c0dd36592c9fca
Nicolas Boulogne-Curriez <nicolas.boulogne-curriez@epitech.eu>
2020-05-20 15:23:42 +0900

    [ADD] Route

--- application/src/services/POST/PostForgotPassword.js
+++ application/src/services/POST/PostForgotPassword.js
@@ -1,13 +1,3 @@
-/**
- * @author Nicolas  BOULOGNE-CURRIEZ <nicolas.boulogne-curriez@epitech.eu>
- * @file Description
- * @desc Created on 2020-01-18 8:24:51 pm
- * @copyright GearStocks
- */
-
-/* eslint-disable no-undef */
-/* eslint-disable no-console */
-
 import React from 'react';
 import { Alert } from 'react-native';
 
@@ -15,10 +5,10 @@ import { routes } from '../../../config/routes';
 
 const axios = require('axios');
 
-export default class PostLogin extends React.Component {
+export default class ForgotPassword extends React.Component {
 
-  login = (JSONObj, navigate) => {
-    axios.post(routes.CONNECT, JSONObj, {
+  forgotPassword = (JSON, navigate) => {
+    axios.post(routes.ForgotPassword, JSON, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Accept': 'application/json',
