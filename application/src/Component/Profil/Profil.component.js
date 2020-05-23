ddc9b81bc82696754910739532f5de30f2ad719a
Nicolas Boulogne-Curriez <nicolas.boulogne-curriez@epitech.eu>
2020-05-23 18:56:35 +0900

    [ADD] Disconnect

--- application/src/Component/Profil/Profil.component.js
+++ application/src/Component/Profil/Profil.component.js
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import { View } from 'react-native';
-import { Text, Button } from 'react-native-elements';
+import { Text, Button, Icon } from 'react-native-elements';
 
 import styles from './Profil.component.style';
 import { strings } from '../../../config/strings';
@@ -36,7 +36,7 @@ export default class ProfilComponent extends React.Component {
     };
   }
 
-  test = function(token, email) {
+  getInfoUser = function(token, email) {
     const JSONObj = JSON.stringify({
       userToken: token,
       mail: email
@@ -54,6 +54,52 @@ export default class ProfilComponent extends React.Component {
         this.setState({ firstname: res.data.firstName });
         this.setState({ lastname: res.data.lastName });
         this.setState({ username: res.data.username });
+        this.setState({ password: res.data.password });
+      })
+      .catch((err) => {
+        console.log(err.name, err.message);
+      });
+  }
+
+  disconnect = function(email, navigation) {
+    const JSONObj = JSON.stringify({
+      mail: email
+    });
+    axios.post(routes.DISCONNECT, JSONObj, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    })
+    .then((res) => {
+      console.log('RESPONSE RECEIVED: ', res.data);
+      this.props.navigation.replace('LoginComponent')
+    })
+    .catch((err) => {
+      console.log(err.name, err.message);
+    });
+  }
+
+  updateInfoUser = function(mail, ) {
+    const JSONObj = JSON.stringify({
+      userToken: token,
+      mail: email
+    });
+    axios.post(routes.INFO_USER, JSONObj, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    })
+      .then((res) => {
+        console.log('RESPONSE RECEIVED: ', res.data);
+        this.setState({ email: res.data.email });
+        this.setState({ firstname: res.data.firstName });
+        this.setState({ lastname: res.data.lastName });
+        this.setState({ username: res.data.username });
+        this.setState({ password: res.data.password });
       })
       .catch((err) => {
         console.log(err.name, err.message);
@@ -61,24 +107,25 @@ export default class ProfilComponent extends React.Component {
   }
 
   componentDidMount() {
-    this.test(this.props.screenProps.token, this.props.screenProps.email);
-  }
-
-  handleClick = () => {
-    <LoginComponent />;
+    this.getInfoUser(this.props.screenProps.token, this.props.screenProps.email);
   }
 
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <View style={styles.container}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontSize: 30, top: 35 }}>GearStocks</Text>
+        <Icon name='format-align-justify' size={30} color='black'
+            containerStyle={{ right: 170, top: 2 }} onPress={() => { this.props.navigation.openDrawer(); }} />
         <Text h4>Welcome {this.state.username}</Text>
         <Text h4>Email : {this.state.email}</Text>
         <Text h4>Username : {this.state.username}</Text>
         <Text h4>Firstname : {this.state.firstname}</Text>
         <Text h4>Lastname : {this.state.lastname}</Text>
+        <Text h4>Password : {this.state.password}</Text>
         <Text h5>
         </Text>
-        <Button title={strings.DISCONNECT} buttonStyle={styles.button} type="outline" onPress={() => this.handleClick()} />
+        <Button title={strings.DISCONNECT} buttonStyle={styles.button} type="outline" onPress={() => this.disconnect(this.state.email, navigate)} />
       </View>
     );
   }
