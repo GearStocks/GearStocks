426001f9c580c9e746d9efa83d5207ae9937b78e
Nicolas Boulogne-Curriez <nicolas.boulogne-curriez@epitech.eu>
2020-05-24 12:50:42 +0900

    [ADD] push after pul

--- front/src/app/auth/services/user.service.ts
+++ front/src/app/auth/services/user.service.ts
@@ -43,6 +43,7 @@ export class UserService {
       password: authData.password,
       rememberMe: authData.rememberMe
     };
+    console.log(this.loginUrl);
     return this.http.post<User>(this.loginUrl, body, httpOptions)
       .pipe(map(user => {
           // login successful if there's a jwt token in the response
