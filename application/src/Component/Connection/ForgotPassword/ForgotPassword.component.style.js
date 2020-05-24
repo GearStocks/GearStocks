9c6be922bd92244049b9f57d63b763c63099bac8
Nicolas Boulogne-Curriez <nicolas.boulogne-curriez@epitech.eu>
2020-05-24 22:30:28 +0900

    [ADD] Forgot Password

--- application/src/Component/Connection/ForgotPassword/ForgotPassword.component.style.js
+++ application/src/Component/Connection/ForgotPassword/ForgotPassword.component.style.js
@@ -1,18 +1,17 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    button: {
-        height: 50,
-        width: 200,
-        borderRadius: 20,
-        backgroundColor: '#050175',
-        elevation: 10,
-        top: 50
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  button: {
+    height: 50,
+    width: 350,
+    borderRadius: 5,
+    top: 30,
+    marginBottom: 20
+  }
 });
