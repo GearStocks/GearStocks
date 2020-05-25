ac9576625f1f17f2e1267ab5493e099413b11bf8
Francois <francoismilhet@hotmail.fr>
2020-05-25 21:09:18 +0200

    update routes

--- front/src/app/core/services/search.service.ts
+++ front/src/app/core/services/search.service.ts
@@ -6,7 +6,7 @@ import { environment } from '../../../environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type':  'application/json',
+    'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
   })
 };
